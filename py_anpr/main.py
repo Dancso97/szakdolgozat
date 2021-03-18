79ca9e5a9b6172f02cf21c54c0b232d141ab69db
Daniel <beke.daniel97@gmail.com>
2021-03-18 23:02:11 +0100

    update

--- py_anpr/main.py
+++ py_anpr/main.py
@@ -1,15 +1,49 @@
 import utils
 from py_anpr_v5 import start_reading
+import time
+
+
+switcher = {
+    1: autoRun
+    2: autoStop
+} 
+
+def autoRun():
+    print("ar")
+
+    '''
+ 
+    bpid = utils.startProcess(utils.BACKEND_RUN)
+    time.sleep(5)
+    backendStatus = utils.checkServerStatus(utils.BACKEND_URL)
+
+    if( backendStatus is True ):
+        utils.startProcess(utils.FRONTEND_RUN)
+        time.sleep(5)
+        frontendStatus = utils.checkServerStatus(utils.FRONTEND_URL)
+    
+    
+    if(backendStatus and frontendStatus):
+        print("both up and running")            
+        #start_reading()
+    '''
+
+
+def autoStop():
+    print("as")
+
 
 def main():
     print("Welcome to ANPR!\n")
-    backendStatus = utils.checkServerStatus(utils.BACKEND_URL)
-    frontendStatus = utils.checkServerStatus(utils.FRONTEND_URL)
-    if(backendStatus and frontendStatus):
-        print("both up and running")
-        
-        
-        #start_reading()
-    
+   
+
+def menu()
+    print("--- Menu ---")
+    print("1,Start engine\n2,Stop engine")
+    choice = input()
+    funcToCall = switcher.get(choice, "Invalid input")
+
+
+
 if __name__ == "__main__":
     main()
--- py_anpr/utils.py
+++ py_anpr/utils.py
@@ -1,8 +1,12 @@
+import sys
 import requests
+import subprocess
 from time import sleep
 
 BACKEND_URL = 'http://localhost:8080/actuator/health'
 FRONTEND_URL = 'http://localhost:3000'
+BACKEND_RUN =  'mvn spring-boot:run -f D:\\Git\\szakdolgozat\\pom.xml'
+FRONTEND_RUN = "npm start --prefix D:\\Git\\szakdolgozat\\prime_frontend\\"
 
 def uploadToServer(pictureName):
     print("In server upload!\n")
@@ -21,3 +25,10 @@ def checkServerStatus(SERVER_URL):
         print("{} is not available\nRetrying....\n".format(SERVER_URL))
         sleep(5)
         checkServerStatus(SERVER_URL)
+
+def startProcess(process):    
+    p = subprocess.Popen(process ,shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    if( p.pid is not None or p.pid > 0 ):
+        print("{} pid: {} ".format(process, str(p.pid)))
+    else:
+        return -1
